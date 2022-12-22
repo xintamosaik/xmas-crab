8f46f0adf3daf012d1d7b9326b8d2e148682e5c5
U1F <xintamosaik@gmail.com>
2022-12-22 09:44:47 +0100

    Change to UI and SVG Co-authored-by: med4kat <med4kat@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //document.body.innerText = "Javascript Works"
 const xmas_grab_run = event => {
-const svgObject = document.getElementById('tree_object').contentDocument
-console.log(svgObject)
-const svg = svgObject.getElementById('the_tree')
-console.log(svg)
+const tree_svgObject = document.getElementById('tree_object')
+const tree_contentDocument =  tree_svgObject.contentDocument
+const tree_svg = tree_contentDocument.getElementById("the_tree")
+console.log(tree_svgObject)
+console.log(tree_contentDocument)
+console.log(tree_svg)
+
+
 }
 document.addEventListener('DOMContentLoaded', xmas_grab_run)
